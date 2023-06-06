3892e90755094d75ba0680d44b5e9762ba4b19dc
Jealous Matsikachando <pasej5@gmail.com>
2023-06-06 16:24:56 -0700

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,7 +2,6 @@
 # 2-print_alphabet.py
 
 """Print the alphabet in lowercase, not followed by a new line."""
-for i in range(ord('a'), ord('z')+1):
-    print(chr(i), end='')
-
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
 
