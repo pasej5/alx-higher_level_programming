b8fa8d2255bfc383c42c5e9e4ee4dcdfd1949264
Jealous Matsikachando <pasej5@gmail.com>
2023-06-06 17:41:05 -0700

    program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
 
-"""Print all possible different combinations of two digits in ascending order.
-
-    The two digits must be different - 01 and 10 are considered identical.
-    """
+"""Write a program that prints all possible different combinations of two digits. """
 for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+    for second_digit in range(digit1 + 1, 10):
+        if digit1 == 8 and second_digit == 9:
+            print("{}{}".format(digit1, second_digit))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
-
-
+            print("{}{}".format(digit1, second_digit), end=", ")
