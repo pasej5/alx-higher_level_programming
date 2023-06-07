e481208008291b77ccc5e9c9ae7eea2e37d9aa24
Jealous Matsikachando <pasej5@gmail.com>
2023-06-06 18:05:59 -0700

    function that checks for lowercase character.def islower(c)

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 """function that checks for lowercase character"""
-def islower(c):
-
+def islower(c)
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
-
