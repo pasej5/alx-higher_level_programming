8f57c569b68262ee017583cf6522228c8ed5f529
Jealous Matsikachando <pasej5@gmail.com>
2023-06-06 17:44:53 -0700

    program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Write a program that prints all possible different combinations of two digits. """
+"""Write a program that prints all possible different
+    combinations of two digits. """
 for digit1 in range(0, 10):
     for second_digit in range(digit1 + 1, 10):
         if digit1 == 8 and second_digit == 9:
