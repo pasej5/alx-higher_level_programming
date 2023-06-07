b965cefab89a7fc5736a9440a6e588f284bd6b50
Jealous Matsikachando <pasej5@gmail.com>
2023-06-06 18:27:21 -0700

    function that prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """function that prints a string in uppercase followed by a new line."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
+
