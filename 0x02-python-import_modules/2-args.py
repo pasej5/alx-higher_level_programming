f7933bec87e3eaecb343c87302b7e7f0853fd5ee
Jealous Matsikachando <pasej5@gmail.com>
2023-06-08 17:54:25 -0700

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import sys
-
-if __name__ == "__main__":
 """ program that prints the number of and the list of its arguments."""
+
+if __name__ == "__main__"
 
     count = len(sys.argv) - 1
     if count == 0:
