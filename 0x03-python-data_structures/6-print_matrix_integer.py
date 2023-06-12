db51d1acf869ae65ed85f02db446f46b605d3f45
Jealous Matsikachando <pasej5@gmail.com>
2023-06-12 00:18:13 -0700

    function that print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-
 def print_matrix_integer(matrix=[[]]):
     """function that prints a matrix of integers"""
     for row in matrix:
-        formated_row = ' '.join([str(num) for num in row ])
-        print(formated_row)
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
