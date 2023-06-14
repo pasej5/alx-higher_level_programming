70d75c849e30840edef1f5bb8a60b0d826789ea9
Jealous Matsikachando <pasej5@gmail.com>
2023-06-14 16:53:59 -0700

    debug retuen statement must be outside the for loop

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,5 @@ def square_matrix_simple(matrix=[]):
 
     for row in range(len(matrix)):
         new_matrix[row] = list(map(lambda x: x**2, matrix[row]))
-        return (new_matrix)
+        
+    return (new_matrix)
