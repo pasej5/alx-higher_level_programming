5fa8424bf939e891257fbb2cd5027dec5d70e586
Jealous Matsikachando <pasej5@gmail.com>
2023-06-14 18:27:14 -0700

    replaces or updates keys or values

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    """prints a dictionary by ordered keys"""
     list_ord = list(a_dictionary.keys())
     list_ord.sort()
     for key in list_ord:
