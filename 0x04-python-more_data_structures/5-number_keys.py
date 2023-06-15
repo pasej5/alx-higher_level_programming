7f1a0b9cc883b85f610275d130a9e6c02ec1a5ea
Jealous Matsikachando <pasej5@gmail.com>
2023-06-14 18:10:06 -0700

    chmod

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,7 +3,7 @@ def number_keys(a_dictionary):
     number = 0
     list_keys = list(a_dictionary.keys())
 
-    for i in list_keys:
+    for key in list_keys:
         number += 1
 
     return (number)
