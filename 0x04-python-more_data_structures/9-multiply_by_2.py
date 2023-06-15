6da68ee47f7667ea558818d98715e53fe3a864d4
Jealous Matsikachando <pasej5@gmail.com>
2023-06-14 19:11:12 -0700

    function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@ def multiply_by_2(a_dictionary):
     new_diction = a_dictionary.copy()
     list_keys = list(new_diction.keys())
 
-    for i in list_keys:
-        new_diction[i] *= 2
+    for key in list_keys:
+        new_diction[key] *= 2
 
     return (new_diction)
