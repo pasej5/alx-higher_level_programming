e5859fe8ccbcbfadb25cc2a7d24a9e1bd2e1e87d
Jealous Matsikachando <pasej5@gmail.com>
2023-06-26 05:53:01 -0700

    return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x = 0)
             print(my_list[], end=' ')
             count += 1
     except IndexError:
-        pass
+        break
     finally:
-        print()
-        return count
+        print("")
+        return (count)
