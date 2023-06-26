bc6429476fe90debf6c96de91348de303d2d549c
Jealous Matsikachando <pasej5@gmail.com>
2023-06-26 05:57:28 -0700

    indantation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x = 0)
-    try :
-        count = 0
-        for i in range(x):
-            print(my_list[], end=' ')
+"""function that prints x elements of a list"""
+   count = 0
+   for i in range(x):
+       try :
+           print(my_list[], end=' ')
             count += 1
     except IndexError:
         break
