13ba2ed1bcaa55e9cc1bc9ac44e6f9aa108da797
pasej5 <pasej5@gmail.com>
2023-06-26 10:28:50 +0000

    betty error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for element in my_list:
             print(element, end="")
             count += 1
-            if count == x 
+            if count == x
                 break
             print()
             return count
