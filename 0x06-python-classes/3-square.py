20dc2b6abda28afa2108bfcfed77625274f7f8c2
Jealous Matsikachando <pasej5@gmail.com>
2023-06-27 18:28:38 -0700

    3-square.py:13:1: W293 blank line contains whitespace

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,6 +10,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
         def area(self):
             """public instance method"""
             return (self.__size * self.__size)
