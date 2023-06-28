148a541775bdbd4984563f113e9fc78a9abdeee3
Jealous Matsikachando <pasej5@gmail.com>
2023-06-27 18:39:17 -0700

    3-square.py:8:31: E231 missing whitespace after ','

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,12 +5,12 @@
 class Square:
     """class Square that defines a square"""
     def __init__(self, size=0):
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-            """public instance method to calculatebthe square"""
+            """method to calculatebthe square"""
             return self.__size * self.__size
