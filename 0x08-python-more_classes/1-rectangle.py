9226b23ba3a8a51504e4b54e635bf6f318150451
Jealous Matsikachando <pasej5@gmail.com>
2023-07-03 10:27:51 -0700

    Pycodestyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,12 +3,17 @@
 
 
 class Rectangle:
-    """Rectangle class"""
+    """Initializing the rectangle"""
 
-    def __init__(self, width=0, height=0):
-        """Initialize a Rectangle instance"""
-        self.__width = width
-        self.__height = height
+    def __init__(self, width=0, height=0)
+    """Initialize a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle (default is 0).
+            height (int):The height of the rectangle (default is 0).
+        """
+    self.width = width
+    self.height = height
 
     @property
     def width(self):
@@ -32,9 +37,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter method for height"""
-        if not isinstance(value, int):
+        if is not isiinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
