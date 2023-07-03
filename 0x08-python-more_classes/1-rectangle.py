73d75345d88e1e06875984fa8c236bf2a3906b62
Jealous Matsikachando <pasej5@gmail.com>
2023-07-03 10:36:03 -0700

    docstring added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,14 @@ class Rectangle:
     """Rectangle class"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a Rectangle instance"""
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle (default is 0).
+            height (int): The height of the rectangle (default is 0).
+        """
+
         self.__width = width
         self.__height = height
 
