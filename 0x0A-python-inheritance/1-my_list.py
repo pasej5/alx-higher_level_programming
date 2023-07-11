569f06465ead71306751e5ad68d0f06e570c38e9
Jealous Matsikachando <pasej5@gmail.com>
2023-07-10 18:22:03 -0700

    syntax err

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ class MyList(list):
     """Class Mylist that inherits from list"""
 
     def print_sorted(self):
-        """method that prints the list, ascending order"""
+        """method that prints list, ascending order"""
 
-        sorted_list == sorted(self)
+        sorted_list = sorted(self)
         print(sorted_list)
