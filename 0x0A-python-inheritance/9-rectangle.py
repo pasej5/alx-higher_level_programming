7f7f323534ec1c44777c3ea3d07d25e6ef28f530
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 01:52:17 -0700

    implementing the area of the rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance with the given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            ValueError: If either width or height is not a positive integer.
+        """
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Computes and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
