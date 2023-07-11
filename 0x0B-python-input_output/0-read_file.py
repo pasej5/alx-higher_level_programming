1445855d153b943019c035cb5082490c72970a3f
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 16:26:28 -0700

    function that reads a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that reads the text (UTF8) and prints it out to stdout:"""
+
+
+def read_file(filename=""):
+    """Function that reads a file"""
+
+
+    with open(filename, encording='utf-8') as file:
+        for line in file:
+            print(line, end='')
