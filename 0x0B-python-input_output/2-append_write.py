6a7f9dd23684a46c5593c0cb0304e1cbb919dd3b
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 17:48:32 -0700

    Alx docstring requirements

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""function that appends a string at the end of a text,
-file utf-8and the returns the number of characters"""
+"""Function that appends a string at the end of a text file
+(UTF-8) and returns the number of characters added."""
 
 
 def append_write(filename="", text=""):
-    """Appends a text file to UTF-8
+    """Appends a string to a text file (UTF-8) and
+    returns the number of characters added.
 
     Args:
-        filename: the name of the file to write.
-        text: the text to write to the file
+        filename: The name of the file to write.
+        text: The text to append to the file.
 
-    Return:
+    Returns:
         The number of characters added.
     """
 
     with open(filename, mode='a', encoding='utf-8') as file:
-    file.write(text)
-    return len(text)
+        file.write(text)
+        return len(text)
