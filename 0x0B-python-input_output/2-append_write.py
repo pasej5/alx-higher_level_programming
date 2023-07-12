35ef5aeb45b166d4be9ad39acd3e060587d18610
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 17:45:13 -0700

    colon error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -14,6 +14,6 @@ def append_write(filename="", text=""):
         The number of characters added.
     """
 
-    with open(filename, mode='a', encoding='utf-8') as file
+    with open(filename, mode='a', encoding='utf-8') as file:
     file.write(text)
     return len(text)
