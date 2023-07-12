6574baeb003fb044f1c68699ecfc87711662fea9
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 19:30:54 -0700

    script that adds all arguments to a Python list,

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Script that adds all arguments to a Python list and save them to a file"""
 import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
 if __name__ == "__main__":
