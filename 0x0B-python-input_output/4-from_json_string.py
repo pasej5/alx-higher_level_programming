4d22e585d5a3be2b911185ba7c7f0d340104b22a
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 18:18:24 -0700

    JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function that returns a Python data structure by JSON string"""
+import json
+
+
+def to_json_string(my_str):
+    """Returns the Python data structure by JSON string
+
+    Args:
+        my_str: The object to be converted to JSON.
+
+    Returns:
+        The JSON python data structure.
+    """
+
+    return json.loads(my_str)
+
