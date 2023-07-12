bc0eca3ca22a5127f4428b80181619e5d03017b1
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 17:01:44 -0700

    syntax err

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,6 @@
 def write_file(filename="", text=""):
     """Writes a text and returns the number of char"""
 
-    def write_file(filename="", text=""):
         with open(filename, mode='w', encoding='utf-8') as file:
             file.write(text)
             return len(text)
