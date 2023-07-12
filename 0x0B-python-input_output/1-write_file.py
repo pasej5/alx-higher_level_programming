569a378fa1fdf957a85ced99d321b3812553dca9
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 17:12:51 -0700

    Docstring change

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,12 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a text fileutf-8 and returns the number of char"""
+    """Writes a text file utf-8 and returns the number of char.
+
+    Args:
+        filename: The name of the file to write.
+        text: The actual text to write to file
+        """
 
         with open(filename, mode='w', encoding='utf-8') as file:
             file.write(text)
