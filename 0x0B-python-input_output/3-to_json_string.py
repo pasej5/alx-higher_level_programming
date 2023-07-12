29c3409f2fd32fac184bf0696fc54d7ab95574a3
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 18:01:40 -0700

    json represantation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that returns the JSON represantation of an object(string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the represantation of an bject string.
+
+    Args:
+        my_obj: My object
+
+    Returns:
+        returns the JSON representation of an object (string)
+    """
+
+    return jason.dumps(my_obj)
