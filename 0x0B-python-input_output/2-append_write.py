c55cd1af358ed2333b306122e434e84dd42a476e
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 17:39:08 -0700

    appending to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that appends a string at the end of a text,
+file utf-8and the returns the number of characters"""
+
+
+def append_write(filename="", text=""):
+    """Appends a text file to UTF-8
+
+    Args:
+        filename: the name of the file to write
+        text: the text to write to the file
+
+    Return:
+        The number of characters added.
+    """
+
+    with open(filename, mode='a', encoding='utf-8') as file
+    file.append(text)
+    return len(text)
