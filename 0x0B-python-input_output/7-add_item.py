cc07c00eb117e1da2d3c8dbcadbca7da66f446b2
Jealous Matsikachando <pasej5@gmail.com>
2023-07-11 19:37:37 -0700

    script that adds all arguments to a Python list,

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and save them to a file"""
+"""Script that adds all arguments to a Python list and saves them to a file"""
 import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_jason_file').save_to_json_file
-    load_from_json_file = \
-            import __import__('6-load_from_json_file').load_from_json_file
+    try:
+        data = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        data = []
 
-try:
-    data = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    data = []
+    data.extend(sys.argv[1:])
 
-data.extend(sys.argv[1:])
+    save_to_json_file(data, 'add_item.json')
 
-save_to_json_file(data, 'add_item.json')
