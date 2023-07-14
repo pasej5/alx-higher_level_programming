dcaff665de7c38c6e53c0726ff7541646289766b
Jealous Matsikachando <pasej5@gmail.com>
2023-07-14 01:06:00 -0700

    overiding the str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(Base):
     def __str__(self):
         """Returning the string reprsentation of the Rectangle"""
 
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self .width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
     @property
     def width(self):
