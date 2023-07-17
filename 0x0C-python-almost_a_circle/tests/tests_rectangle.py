2fbcf7dd60daa34179d4f3b445951788d073042e
pasej5 <pasej5@gmail.com>
2023-07-17 03:16:01 +0000

    tests

---
+++ 0x0C-python-almost_a_circle/tests/tests_rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Tests for Rectangle.py"""
+
+
+import unittest
+from rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def test_area(self):
+        # Test the area calculation
+        pass
+
+    def test_display(self):
+        # Test the display method
+        pass
+
+    def test_update(self):
+        # Test the update method
+        pass
+
+    def test_to_dictionary(self):
+        # Test the to_dictionary method
+        pass
+if __name__ == '__main__':
+    unittest.main()
