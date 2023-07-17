ba06a99ec385f7a84d65a5739128a1a0cef43ddf
Jealous Matsikachando <pasej5@gmail.com>
2023-07-17 01:03:12 -0700

    rectangle test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Tests for base.py"""
-
-
 import unittest
 from models.base import Base
+
 
 class TestBase(unittest.TestCase):
     def test_to_json_string(self):
@@ -26,7 +25,22 @@ class TestBase(unittest.TestCase):
         expected = [{'id': 1, 'name': 'Nopah'}, {'id': 2, 'name': 'Matsika'}]
         self.assertEqual(result, expected)
 
-    def test_save_to_file(self)
+    def test_save_to_file(self):
+        """Test saving an empty list of objects"""
+        Base.save_to_file([])
+
+    def test_create(self):
+        """Test creating and instance Rectangle and square"""
+        rectangle_dict = {'id': 1, 'width': 10, 'height': 20}
+        rectangle = Base.create(**rectangle_dict)
+
+        square_dict = {'id': 2, 'size': 5}
+        square = Base.create(**square_dict)
+
+    def test_load_from_file(self):
+        """Test loading from a non-existent file"""
+        result = Base.load_from_file()
+        self.assertEqual(result, [])
 
 
 if __name__ == '__main__':
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Tests for rectangle.py"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def test_area(self):
+        """Test the area() method of Rectangle"""
+        # Test code for the area() method
+
+    def test_display(self):
+        """Test the display() method of Rectangle"""
+        # Test code for the display() method
+
+    def test_update(self):
+        """Test the update() method of Rectangle"""
+        # Test code for the update() method
+
+    # Add more test methods for other methods/attributes of Rectangle
+
+
+if __name__ == '__main__':
+    unittest.main()
