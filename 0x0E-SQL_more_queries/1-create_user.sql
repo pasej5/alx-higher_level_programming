ea9e20228c9c208fb90c4bc74fb174e9c61862e3
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 17:19:51 -0700

    script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the MySQL server user user_0d_1
+
+CREATE USER IF NOT EXISTS 'user_0d-1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1''@localhost';
+FLUSH PRIVILEGES;
