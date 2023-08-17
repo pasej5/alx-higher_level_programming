2ec1399b7c59fe52d2720092adbecb67b43166b5
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 19:57:21 -0700

    script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- USE hbtn_0d_tvshows;
+-- Second column must be called number_of_shows
+-- Don’t display a genre that doesn’t have any shows linked
+
+SELECT genres.genre AS genre, COUNT(tv_show_genres.id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.genre
+ORDER BY number_of_shows DESC;
