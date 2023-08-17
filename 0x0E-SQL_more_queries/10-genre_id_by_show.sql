ef58e7c0e8dd6de3cea42846ede55534a594d7d1
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 19:39:51 -0700

    script that lists all shows contained in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,8 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT t.title, tg.genre_id
+FROM tv_shows AS `t`
+ INNER JOIN tv_show_genres AS `tg`
+	ON t.id = tg.show_id
+ORDER BY t.title, tg.genre_id;
