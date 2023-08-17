a9d10a08249d2ba7ae6bc9fb8c3a3323c39434a2
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 19:50:46 -0700

    script that lists all shows contained in hbtn_0d_tvshows without a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+Each record should display: tv_shows.title - tv_show_genres.genre_id
+Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+-- USE hbtn_0d_tvshows;
+
+SELECT tv_shows.title, NULL AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC;
