8ebb8d27343658a2dd0a9b6213772cf46d38fa85
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 19:31:10 -0700

    typo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,8 +3,11 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 -- USE hbtn_0d_tvshows;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN (
+    SELECT DISTINCT tv_show_id, genre_id
+    FROM tv_show_genres
+) AS tv_show_genres
+ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
