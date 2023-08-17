3a26831a258b86b2e49a9735136c8639f84b5557
Jealous Matsikachando <pasej5@gmail.com>
2023-08-16 18:38:05 -0700

    If the table force_name already exists, your script should not fail

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,8 @@
 -- Script that creates the table force_name on your MySQL server
+-- name VARCHAR(256) can’t be null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+
 
 CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
