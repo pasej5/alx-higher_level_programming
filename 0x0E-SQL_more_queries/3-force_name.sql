706c3a2935fe798d6fd1b43521c47956bc5e3ab9
Jealous Matsikachando <pasej5@gmail.com>
2023-08-17 05:42:43 -0700

    typo

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -3,7 +3,6 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
 
-
 CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
