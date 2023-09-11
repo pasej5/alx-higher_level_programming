0374ba88fcea0a298fa51df0050df38e62974332
Jealous Matsikachando <pasej5@gmail.com>
2023-09-11 13:55:30 -0700

    chmod fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-console.log("No arguments");
-} else if (process.argv.length ===3) {
-console.log("Arguments found");
+  console.log('No arguments');
+} else if (process.argv.length === 3) {
+  console.log('Arguments found');
 } else {
-console.log("Arguments found");
+  console.log('Arguments found');
 }
