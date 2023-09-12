c37e6971312a5922144ae13027433d3da8408737
Jealous Matsikachando <pasej5@gmail.com>
2023-09-11 17:47:04 -0700

    script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  const sum = num1 + num2;
-  console.log(sum);
-} else {
-  console.log("Invalid input. Please provide two valid integers.");
+function add (a, b) {
+  return a + b;
 }
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+const sum = add(num1, num2);
+console.log(sum);
