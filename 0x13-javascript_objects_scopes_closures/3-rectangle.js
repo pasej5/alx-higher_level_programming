c3edc399f7c23c40196b1ca2ad1131176a70a726
Jealous Matsikachando <pasej5@gmail.com>
2023-09-13 15:39:28 -0700

    creating a rectangle instance

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
+
 class Rectangle {
-  constructor (w, h) {
-  if (w <= 0 || h <= 0 typeof w !== 'number' || typeof h !== 'number' {
-  return {};
-  }   
-   
-   this.width = w;
-   this.height = h;
-  
-  print() {
-  for (let i = 0; i < this.height; i++){
-    let row = '';
-    for (let j = 0; j < this.width; j++) {
-      row += 'X';
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
     }
-    console.log(row);
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
   }
 }
