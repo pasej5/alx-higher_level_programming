a2499e39af9589cbe256e7e5b99ad0daf5102d40
Jealous Matsikachando <pasej5@gmail.com>
2023-09-22 18:55:59 -0700

    id that represents a column of an auto-generated, unique integer, can’t be null and is a primary

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""
- a script prints all City objects from the database hbtn_0e_14_usa:
-
-"""
-
+"""lists all City objects from the database hbtn_0e_14_usa """
 import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-# should not be executed when imported
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(City, State).filter(State.id == City.state_id).all()
-    for state, city in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-
-    session.commit()
+    cities = session.query(State.name, City.id, City.name)\
+                    .filter(State.id == City.state_id)
+    for elmt in cities:
+        print(f"{elmt[0]}: ({elmt[1]}) {elmt[2]}")
