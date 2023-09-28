c8a293b224cf3aace419a293525d8b4c8638df15
Jealous Matsikachando <pasej5@gmail.com>
2023-09-28 13:01:17 -0700

    documantation fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL
+#Bash script that takes in a URL
 #displays the size of the body of the response of a given url
 #sends a request to that URL
 curl -s "$1" | wc -c
