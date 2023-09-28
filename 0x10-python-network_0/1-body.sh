b26bc2dbbc8d3e9ed72f8e6251bb5c5d2e0362b1
Jealous Matsikachando <pasej5@gmail.com>
2023-09-28 13:23:58 -0700

    displays the size of the body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Displays the size of the body of the response of a given URL with a 200 status code
-
-response=$(curl -s -w "%{http_code}" "$1")
-
-# Check if the response code is 200
-if [ "$response" == "200" ]; then
-    curl -s "$1" | sed -n '/^$/,$p' | tail -n +2
-else
-    echo "The response status code is not 200."
-fi
+#Bash script sending a request and displaying the size in bytes
+curl -sL "$1"
