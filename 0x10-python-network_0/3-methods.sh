0fa6b9dca770ebc780b4a394b73e41737736292b
Jealous Matsikachando <pasej5@gmail.com>
2023-09-28 15:43:05 -0700

    displays all HTTP methods the server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This line is a shebang that specifies to use the Bash interpreter to run the script.
-
-# Takes in a URL and displays all HTTP methods the server will accept
-curl -s -X OPTIONS -i "$1" | grep -i "allow" | tr -d '\r' | awk -F ': ' '{print $2}'
+#takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | sed -e 's/Allow: //i'
