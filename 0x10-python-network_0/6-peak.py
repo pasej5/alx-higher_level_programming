fe97c7b5797f4821360baf2248e6b749c9691d8a
Jealous Matsikachando <pasej5@gmail.com>
2023-09-28 16:48:53 -0700

    finding the peak of unsorted numbers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """Peak of unsorted numbers"""
 
-
 def find_peak(list_of_integers):
-    """Finding the peak of unsorted intergers"""
-    prev = 0
-    for idx, num in enumerate(list_of_integers):
-        if idx:
-            prev = list_of_integers[idx - 1]
-        if idx < len(list_of_integers) - 1:
-            next = list_of_integers[idx + 1]
+    """Findind the peak of unsorted numbers"""
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            right = mid
         else:
-            next = 0
-        if num >= prev and num >= next:
-            return num
+            left = mid + 1
+
+    return list_of_integers[left]
