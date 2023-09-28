cfeeba7bcbe40c16151e98992de24c110e3331c7
Jealous Matsikachando <pasej5@gmail.com>
2023-09-28 16:03:23 -0700

    all methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays all HTTP methods the server 
-curl -s -X OPTIONS -i "$1" | grep -i "allow" | tr -d '\r' | awk -F ': ' '{print $2}'
+curl -siX OPTIONS "$1" | awk -F ': ' '/^Allow:/ {print $2}'
