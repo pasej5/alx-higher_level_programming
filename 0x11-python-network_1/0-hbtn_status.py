eb9664445cded3b403b093831668c91620a121cf
Jealous Matsikachando <pasej5@gmail.com>
2023-09-29 08:46:29 +0000

    script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import urllib.request
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response
+html = response.read
