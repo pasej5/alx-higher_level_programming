3c367c9fc336a0e8d695b301a125d5801debd726
Jealous Matsikachando <pasej5@gmail.com>
2023-09-29 12:04:05 +0000

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-import urllib.request
-"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+mport urllib.request
+
+# Python script that fetches https://alx-intranet.hbtn.io/status
 
 url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response
-html = response.read().decode('utf-8')
+with urllib.request.urlopen(url) as response:
+    html = response.read().decode('utf-8')
