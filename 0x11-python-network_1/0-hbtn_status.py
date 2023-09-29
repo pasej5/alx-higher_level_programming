00154698ca0b3235d350ba4733b21f51c35a4f83
Jealous Matsikachando <pasej5@gmail.com>
2023-09-29 12:26:43 +0000

    python script that gets a website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""script to fetch a website"""
+"""Python script that fetches a website"""
 
 import urllib.request
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
-        r = f.read()
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode('utf-8')
+    
     print('Body response:')
-    print('\t- type: {}'.format(type(r)))
-    print('\t- content: {}'.format(r))
-    print('\t- utf8 content: {}'.format(r.decode('utf-8')))
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
