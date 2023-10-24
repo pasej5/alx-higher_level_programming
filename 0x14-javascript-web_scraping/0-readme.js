0f7068d0549375f36ba28a3de0e3eafe0bdfe905
Jealous Matsikachando <pasej5@gmail.com>
2023-10-24 14:51:31 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-const readMe = require('redMe');
-readMe.readFile(process.argv[2]. 'utf8', function (error, content) {
-console.log(error || content);
-});
+const fs = require('fs');
 
+if (process.argv.length < 3) {
+  console.log('Usage: node yourScriptName.js <filename>');
+  process.exit(1);
+}
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', function (error, content) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(content);
+  }
+});
