1fe6c9f00944f4e37a408f97b24f1bf463b1d6a1
Jealous Matsikachando <pasej5@gmail.com>
2023-10-24 18:17:43 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length < 4) {
-  console.log('Usage: node writeToFile.js <file_path> <content>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf8', function (error) {
-  if (error) {
-    console.error('Error writing to the file:', error);
-  } else {
-    console.log('Content successfully written to the file.');
-  }
+fs.writeFile(process.argv[2], process.agv[3], error => {
+   if (error) console.log(error);
 });
