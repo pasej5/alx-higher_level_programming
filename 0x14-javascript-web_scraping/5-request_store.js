498f049ba23965ef56caf8fd537fdebcf0b899d9
Jealous Matsikachando <pasej5@gmail.com>
2023-10-25 01:27:23 -0700

    smistarndard fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(processargv[3]));
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
