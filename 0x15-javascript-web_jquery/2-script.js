90266142130f3d6eda214537cd16bae822f66dbe
Jealous Matsikachando <pasej5@gmail.com>
2023-11-02 15:22:49 -0700

    syntax error

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
-document.readyState(function () {
-  $('DIV#red_header').click(function () {
-    $('header'.css('color', '#FF0000'));
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('header').css('color', '#FF0000');
   });
 });
